da9102c4e6b182ff9a1a27e494a3545732b15b2a
Zhong Zhenyu <nczzy1997@gmail.com>
2023-04-11 23:05:07 +0800

    Implement DeepSpeed training

---
+++ training/train-ds.py
@@ -0,0 +1,161 @@
+import torch
+import pathlib
+import argparse
+
+from typing import *
+
+import mcpt
+import mcpt.records
+
+import deepspeed
+
+
+def int_or_str(value: str) -> Union[int, str]:
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+def cosine_annealing_warmup(
+        config: Dict[str, Any],
+        n_ctx: int,
+        dp_size: int = 1,
+        alpha: float = 0.1,
+        last_epoch: int = -1,
+) -> Callable[[torch.optim.Optimizer], torch.optim.lr_scheduler.LRScheduler]:
+    def _get_lr_scheduler(optimizer_):
+        return mcpt.train.schedulers.cosine_annealing_warmup(
+            optimizer_,
+            config=config,
+            n_ctx=n_ctx,
+            dp_size=dp_size,
+            alpha=alpha,
+            last_epoch=last_epoch,
+        )
+
+    return _get_lr_scheduler
+
+
+class DSModelCheckpointCallback(mcpt.train.callbacks.ModelCheckpointCallback):
+
+    def __call__(
+            self,
+            model: deepspeed.DeepSpeedEngine,
+            epoch: int,
+            loss: float,
+            batch: Optional[int] = None,
+            val_loss: Optional[float] = None,
+            end_of_epoch: bool = False,
+    ):
+        save_name = self._get_save_name(batch=batch, val_loss=val_loss)
+        save_name = save_name.format(epoch=epoch, batch=batch, loss=loss, val_loss=val_loss)
+        if self.save_frequency == 'epoch' and not end_of_epoch:
+            return
+        if isinstance(self.save_frequency, int):
+            if end_of_epoch or batch % self.save_frequency != 0:
+                return
+        model.save_checkpoint(str(self.save_path), tag=save_name[:-3])
+
+
+# noinspection PyTypeChecker
+def main(cmd_args: argparse.Namespace):
+    hvd = mcpt.stubs.Horovod(
+        size=deepspeed.comm.comm.get_world_size(),
+        rank=deepspeed.comm.comm.get_rank(),
+        local_rank=deepspeed.comm.comm.get_local_rank(),
+    )
+    with mcpt.running('Loading configs', hvd=hvd) as spinner:
+        model_config = mcpt.load_config(cmd_args.model_config)
+        training_config = mcpt.load_config(cmd_args.training_config)
+        ds_config = mcpt.load_config(cmd_args.deepspeed_config)
+        save_path = pathlib.Path(cmd_args.save_path)
+        spinner.write(cmd_args)
+        spinner.write(model_config)
+        spinner.write(training_config)
+
+    with mcpt.running('Loading the dataset', hvd=hvd, timer=True):
+        train_loader = mcpt.records.load(
+            path=cmd_args.training_data,
+            meta=cmd_args.training_meta,
+            batch_size=ds_config['train_micro_batch_size_per_gpu'],
+            dp_size=hvd.size(),
+            dp_rank=hvd.rank(),
+            use_pinyin=model_config.get('use_pinyin', False),
+        )
+
+    with mcpt.running('Loading the model', hvd=hvd, timer=True):
+        model = mcpt.Model.from_config(
+            config=model_config,
+            load_model=cmd_args.load_model,
+        )
+
+        lr_scheduler = cosine_annealing_warmup(
+            config={
+                'batch_size': ds_config['train_micro_batch_size_per_gpu'],
+                'warmup_tokens': training_config['warmup_tokens'],
+                'decay_tokens': training_config['decay_tokens'],
+            },
+            n_ctx=model.config['n_ctx'],
+            dp_size=hvd.size(),
+        )
+        model_engine, optimizer, _, _ = deepspeed.initialize(
+            args=cmd_args,
+            model=model,
+            model_parameters=model.parameters(),
+            lr_scheduler=lr_scheduler,
+        )
+        callbacks = []
+        for freq in cmd_args.save_frequency:
+            if isinstance(freq, str):
+                assert freq in ('epoch', 'step')
+            callbacks.append(
+                DSModelCheckpointCallback(
+                    save_path=save_path,
+                    save_frequency=freq,
+                ),
+            )
+
+    if hvd.rank() == 0:
+        print(model)
+
+    for epoch in range(1, training_config['epochs'] + 1):
+        train_tqdm = mcpt.tqdm(enumerate(train_loader), total=len(train_loader), hvd=hvd)
+        loss = 0
+        for batch_idx, (data, target) in train_tqdm:
+            data, target = data.to('cuda'), target.to('cuda')
+            logits, present = model_engine(data)
+            loss = torch.nn.functional.cross_entropy(logits.permute(0, 2, 1), target, ignore_index=0)
+            model_engine.backward(loss)
+            model_engine.step()
+            if hvd.rank() == 0 and (batch_idx + 1) % cmd_args.log_frequency == 0:
+                train_tqdm.write(
+                    f'Train Epoch: {1}/{training_config["epochs"]} [{batch_idx + 1}/{len(train_loader)}] '
+                    f'Loss: {loss.item()}'
+                )
+            for callback in callbacks:
+                callback(model=model_engine, epoch=epoch, batch=batch_idx + 1, loss=loss)
+        for callback in callbacks:
+            callback(
+                model=model_engine,
+                epoch=epoch,
+                loss=loss,
+                end_of_epoch=True,
+            )
+
+
+if __name__ == '__main__':
+    mcpt.init()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--model_config', type=str, required=True)
+    parser.add_argument('--training_config', type=str, required=True)
+    parser.add_argument('--training_data', type=str, required=True)
+    parser.add_argument('--training_meta', type=str, default='train-meta.pkl')
+    parser.add_argument('--load_model', type=str)
+    parser.add_argument('--save_path', type=str, default='./ckpt')
+    parser.add_argument('--log_frequency', type=int, default=10)
+    parser.add_argument('--save_frequency', type=int_or_str, nargs='+', default=['epoch'])
+    parser = deepspeed.add_config_arguments(parser)
+    deepspeed.init_distributed()
+    main(parser.parse_args())
