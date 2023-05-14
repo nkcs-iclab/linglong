4fbc1647fa326d109ccf8215d081d93b6e947c6a
eye1patch <1263462887@qq.com>
2023-05-14 21:25:19 +0800

    Add word-level tokenizer

--- mcpt/tokenization.py
+++ mcpt/tokenization.py
@@ -2,6 +2,8 @@ import string
 import pathlib
 import pypinyin
 import collections
+import six
+import jieba
 
 from transformers.tokenization_utils import PreTrainedTokenizer
 from transformers.models.bert.tokenization_bert import (
@@ -232,3 +234,174 @@ class BasicTokenizer(BertBasicTokenizer):
             else:
                 output.append(char)
         return ''.join(output)
+
+
+def convert_to_unicode(text):
+  """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
+  if six.PY3:
+    if isinstance(text, str):
+      return text
+    elif isinstance(text, bytes):
+      return text.decode("utf-8", "ignore")
+    else:
+      raise ValueError("Unsupported string type: %s" % (type(text)))
+  elif six.PY2:
+    if isinstance(text, str):
+      return text.decode("utf-8", "ignore")
+    elif isinstance(text, unicode):
+      return text
+    else:
+      raise ValueError("Unsupported string type: %s" % (type(text)))
+  else:
+    raise ValueError("Not running on Python2 or Python 3?")
+
+
+def load_vocab(vocab_file):
+  """Loads a vocabulary file into a dictionary."""
+  vocab = collections.OrderedDict()
+  index = 0
+  with open(vocab_file, "r") as reader:
+    while True:
+      token = convert_to_unicode(reader.readline())
+      if not token:
+        break
+      token = token.strip()
+      vocab[token] = index
+      index += 1
+  return vocab
+
+
+class CPM2WordpieceTokenizer(object):
+
+    def __init__(self, vocab, unk_token="<unk>", max_input_chars_per_word=200):
+        self.vocab = vocab
+        self.unk_token = unk_token
+        self.max_input_chars_per_word = max_input_chars_per_word
+
+    def tokenize(self, token):
+
+        token = convert_to_unicode(token)
+
+        chars = list(token)
+        if len(chars) > self.max_input_chars_per_word:
+            return [self.unk_token]
+
+        start = 0
+        sub_tokens = []
+        while start < len(chars):
+            end = len(chars)
+            cur_substr = None
+            while start < end:
+                substr = "".join(chars[start:end])
+                if substr in self.vocab:
+                    cur_substr = substr
+                    break
+                end -= 1
+            if cur_substr is None:
+                sub_tokens.append(self.unk_token)
+                start += 1
+                continue
+            sub_tokens.append(cur_substr)
+            start = end
+
+        return sub_tokens
+
+
+def Q2B(uchar):  # 将全角字符转换为半角字符
+    if uchar in ['，', '。', '！', '（', '）', '？', '、', '；', '：']:
+        return uchar
+    inside_code = ord(uchar)
+    if inside_code == 0x3000:
+        inside_code = 0x0020
+    else:
+        inside_code -= 0xfee0
+    if inside_code < 0x0020 or inside_code > 0x7e: 
+        return uchar
+    return chr(inside_code)
+
+
+class CPM2EncDecTokenizer(object):
+
+    def __init__(self, vocab_file, max_len=None, max_sentinels=190):
+        self.max_len = max_len if max_len is not None else int(1e12)
+        self.encoder = load_vocab(vocab_file)
+        self.decoder = {v:k for k,v in self.encoder.items()}
+        self.wordpiece_tokenizer = CPM2WordpieceTokenizer(vocab=self.encoder)
+
+        self.translator = str.maketrans(" \n", "\u2582\u2583")
+
+        self.sentinel_list = [self.encoder['<s_{}>'.format(i)] for i in range(max_sentinels)]
+
+    @property
+    def vocab_size(self):
+        return len(self.encoder)
+
+    def __len__(self):
+        return len(self.encoder)
+
+    @property
+    def eod_id(self):
+        return self.encoder[self.eod_token]
+
+    @property
+    def pad_id(self):
+        return self.encoder[self.pad_token]
+
+    @property
+    def mask_id(self):
+        return self.encoder[self.mask_token]
+
+    @property
+    def eod_token(self):
+        return '<eod>'
+
+    @property
+    def pad_token(self):
+        return '<pad>'
+
+    @property
+    def mask_token(self):
+        return '<mask>'
+
+    def get_sentinel_num(self):
+        return len(self.sentinel_list)
+
+    def get_sentinel_id(self, idx):
+        return self.sentinel_list[idx]
+
+    def tokenize(self, text):
+        """ Tokenize a string. """
+        text = ''.join([Q2B(x) for x in text])
+        output_tokens = []
+        for x in jieba.cut(text, cut_all=False):
+            x = x.translate(self.translator)
+            output_tokens.extend(self.wordpiece_tokenizer.tokenize(x))
+        return output_tokens
+
+    def encode(self, text):
+        # text = ''.join([Q2B(x) for x in text])
+        res = [self.encoder[x] for x in self.tokenize(text)]
+        return res
+
+    def decode(self, tokens):
+        text = ''.join([self.decoder[x] for x in tokens])
+        text = text.replace('\u2582', ' ').replace('\u2583', '\n')
+        return text
+
+    def convert_tokens_to_ids(self, tokens):
+        if isinstance(tokens, list):
+            return [self.encoder.get(x, self.encoder["<unk>"]) for x in tokens]
+        elif isinstance(tokens, str):
+            return self.encoder.get(tokens, self.encoder["<unk>"])
+
+    def convert_ids_to_tokens(self, ids):
+        return [self.decoder[x] for x in ids]
+    
+    def convert_ids_to_string(self, ids):
+        if isinstance(ids, tf.Tensor):
+            ids = ids.numpy()
+        return "".join([self.decoder[x] for x in ids])
+    
+    def convert_string_to_ids(self, text):
+        return self.convert_tokens_to_ids(self.tokenize(text))
+
