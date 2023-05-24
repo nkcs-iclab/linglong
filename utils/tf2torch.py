d6abc1c90571d9cc63d92a1a6947656c85c8534f
Zhong Zhenyu <nczzy1997@gmail.com>
2023-05-24 15:33:27 +0800

    Refactor tf2torch.py

--- utils/tf2torch.py
+++ utils/tf2torch.py
@@ -1,6 +1,7 @@
 import fire
 import torch
 import pathlib
+import tensorflow as tf
 
 from typing import *
 
@@ -8,28 +9,8 @@ import mcpt_tf
 import mcpt
 
 
-def load_tf_model(model_config: str, path: str) -> mcpt_tf.Model:
-    model = mcpt_tf.Model.from_config_(model_config, load_model=path)
-    return model
-
-
-def create_torch_model(model_config: str) -> mcpt.Model:
-    model = mcpt.Model.from_config(model_config)
-    return model
-
-
-def find_tf_weight(weights, key: str):
-    for weight in weights:
-        if weight.name == key:
-            return weight
-
-
-def set_weight(torch_key: str, tf_key: str, torch_weights, tf_weights):
-    try:
-        tf_weight = find_tf_weight(tf_weights, tf_key).numpy()
-    except AttributeError as e:
-        print(f'Not found: {tf_key}')
-        raise e
+def set_weight(torch_key: str, tf_key: str, torch_weights: Dict[str, torch.Tensor], tf_weights: Dict[str, tf.Variable]):
+    tf_weight = tf_weights[tf_key].numpy()
     if list(torch_weights[torch_key].size()) != list(tf_weight.shape):
         tf_weight = tf_weight[0]
     assert list(torch_weights[torch_key].size()) == list(tf_weight.shape)
@@ -37,81 +18,146 @@ def set_weight(torch_key: str, tf_key: str, torch_weights, tf_weights):
     torch_weights[torch_key] = torch.from_numpy(tf_weight)
 
 
-def tf_idx(idx: int):
+def tf_idx(idx: int) -> str:
     return f'_{idx}' if idx > 0 else ''
 
 
-def main(model_config: str, tf_model: str, torch_model: Optional[str] = None):
-    tf_model_ = load_tf_model(model_config, tf_model)
-    torch_model_ = create_torch_model(model_config)
-    tf_weights = tf_model_.weights
-    print(f'Loaded {len(tf_weights)} weights from {tf_model}: {[w.name for w in tf_weights]}')
-    torch_weights = torch_model_.state_dict()
-    print(f'Loaded {len(torch_weights)} weights from {torch_model}: {[w for w in torch_weights]}')
+def main(
+        model_config: str,
+        tf_model_path: str,
+        torch_model_path: Optional[str] = None,
+):
+    tf_model = mcpt_tf.Model.from_config_(model_config, load_model=tf_model_path)
+    torch_model = mcpt.Model.from_config(model_config)
 
-    set_weight('transformer.wte.weight', 'mcpt_model/embedding/embedding_1/embeddings:0', torch_weights, tf_weights)
-    set_weight('transformer.wpe.weight', 'Variable:0', torch_weights, tf_weights)
+    tf_weights = tf_model.weights
+    tf_weights = {w.name: w for w in tf_weights}
+    print(f'Loaded {len(tf_weights)} weights from the TensorFlow model: '
+          f'{mcpt.pprint(list(tf_weights.keys()), export=True)}')
+
+    torch_weights = torch_model.state_dict()
+    print(f'Loaded {len(torch_weights)} weights from the PyTorch model: '
+          f'{mcpt.pprint(list(torch_weights.keys()), export=True)}')
+
+    set_weight(
+        'transformer.wte.weight',
+        'mcpt_model/embedding/embedding_1/embeddings:0',
+        torch_weights,
+        tf_weights,
+    )
+    set_weight(
+        'transformer.wpe.weight',
+        'Variable:0',
+        torch_weights,
+        tf_weights,
+    )
 
     ln_idx = 0
     attn_idx = 0
     mlp_idx = 0
     conv1d_idx = 0
-    for i in range(torch_model_.config['n_layer']):
-        set_weight(f'transformer.blocks.{i}.ln_1.weight',
-                   f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/gamma:0', torch_weights,
-                   tf_weights)
-        set_weight(f'transformer.blocks.{i}.ln_1.bias',
-                   f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/beta:0', torch_weights, tf_weights)
+    for i in range(torch_model.config['n_layer']):
+        set_weight(
+            f'transformer.blocks.{i}.ln_1.weight',
+            f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/gamma:0',
+            torch_weights,
+            tf_weights,
+        )
+        set_weight(
+            f'transformer.blocks.{i}.ln_1.bias',
+            f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/beta:0',
+            torch_weights,
+            tf_weights,
+        )
         ln_idx += 1
 
-        set_weight(f'transformer.blocks.{i}.attn.c_attn.w',
-                   f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
-                   torch_weights, tf_weights)
-        set_weight(f'transformer.blocks.{i}.attn.c_attn.b',
-                   f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
-                   torch_weights, tf_weights)
+        set_weight(
+            f'transformer.blocks.{i}.attn.c_attn.w',
+            f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
+            torch_weights,
+            tf_weights,
+        )
+        set_weight(
+            f'transformer.blocks.{i}.attn.c_attn.b',
+            f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
+            torch_weights,
+            tf_weights,
+        )
         conv1d_idx += 1
 
-        set_weight(f'transformer.blocks.{i}.attn.c_proj.w',
-                   f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
-                   torch_weights, tf_weights)
-        set_weight(f'transformer.blocks.{i}.attn.c_proj.b',
-                   f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
-                   torch_weights, tf_weights)
+        set_weight(
+            f'transformer.blocks.{i}.attn.c_proj.w',
+            f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
+            torch_weights,
+            tf_weights,
+        )
+        set_weight(
+            f'transformer.blocks.{i}.attn.c_proj.b',
+            f'mcpt_model/block{tf_idx(i)}/attention{tf_idx(attn_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
+            torch_weights,
+            tf_weights,
+        )
         attn_idx += 1
         conv1d_idx += 1
 
-        set_weight(f'transformer.blocks.{i}.ln_2.weight',
-                   f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/gamma:0', torch_weights,
-                   tf_weights)
-        set_weight(f'transformer.blocks.{i}.ln_2.bias',
-                   f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/beta:0', torch_weights, tf_weights)
+        set_weight(
+            f'transformer.blocks.{i}.ln_2.weight',
+            f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/gamma:0',
+            torch_weights,
+            tf_weights,
+        )
+        set_weight(
+            f'transformer.blocks.{i}.ln_2.bias',
+            f'mcpt_model/block{tf_idx(i)}/layer_normalization{tf_idx(ln_idx)}/beta:0',
+            torch_weights,
+            tf_weights,
+        )
         ln_idx += 1
 
-        set_weight(f'transformer.blocks.{i}.mlp.c_fc.w',
-                   f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
-                   torch_weights, tf_weights)
-        set_weight(f'transformer.blocks.{i}.mlp.c_fc.b',
-                   f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
-                   torch_weights, tf_weights)
+        set_weight(
+            f'transformer.blocks.{i}.mlp.c_fc.w',
+            f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
+            torch_weights,
+            tf_weights,
+        )
+        set_weight(
+            f'transformer.blocks.{i}.mlp.c_fc.b',
+            f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
+            torch_weights,
+            tf_weights,
+        )
         conv1d_idx += 1
-        set_weight(f'transformer.blocks.{i}.mlp.c_proj.w',
-                   f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
-                   torch_weights, tf_weights)
-        set_weight(f'transformer.blocks.{i}.mlp.c_proj.b',
-                   f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
-                   torch_weights, tf_weights)
+        set_weight(
+            f'transformer.blocks.{i}.mlp.c_proj.w',
+            f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_w:0',
+            torch_weights,
+            tf_weights,
+        )
+        set_weight(
+            f'transformer.blocks.{i}.mlp.c_proj.b',
+            f'mcpt_model/block{tf_idx(i)}/mlp{tf_idx(mlp_idx)}/conv1d{tf_idx(conv1d_idx)}/conv1d_b:0',
+            torch_weights,
+            tf_weights,
+        )
         mlp_idx += 1
         conv1d_idx += 1
 
-    set_weight('transformer.ln_f.weight', f'mcpt_model/layer_normalization{tf_idx(ln_idx)}/gamma:0', torch_weights,
-               tf_weights)
-    set_weight('transformer.ln_f.bias', f'mcpt_model/layer_normalization{tf_idx(ln_idx)}/beta:0', torch_weights,
-               tf_weights)
-    torch_model_.load_state_dict(torch_weights)
-    if torch_model is None:
-        torch_model = pathlib.Path(tf_model).with_suffix('.pt')
-    torch.save(torch_weights, torch_model)
+    set_weight(
+        'transformer.ln_f.weight',
+        f'mcpt_model/layer_normalization{tf_idx(ln_idx)}/gamma:0',
+        torch_weights,
+        tf_weights,
+    )
+    set_weight(
+        'transformer.ln_f.bias',
+        f'mcpt_model/layer_normalization{tf_idx(ln_idx)}/beta:0',
+        torch_weights,
+        tf_weights,
+    )
+    torch_model.load_state_dict(torch_weights)
+    if torch_model_path is None:
+        torch_model_path = pathlib.Path(tf_model_path).with_suffix('.pt')
+    torch.save(torch_weights, torch_model_path)
 
 
 if __name__ == '__main__':
