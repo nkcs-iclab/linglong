1ee42b6824152333e52654d4707c10ffdf010b4d
Zhong Zhenyu <nczzy1997@gmail.com>
2023-07-28 16:28:47 +0800

    Use class methods to create models from configs

--- mcpt/models/model.py
+++ mcpt/models/model.py
@@ -14,19 +14,20 @@ class Model(nn.Module):
     def forward(self, inputs, past=None):
         return self.transformer(inputs, past=past)
 
-    @staticmethod
+    @classmethod
     def from_config(
+            cls,
             config: Union[str, Dict[str, Any]],
             load_model: Optional[str] = None,
             device: Optional = None,
-    ) -> 'Model':
+    ) -> 'Self':
         import mcpt
         if isinstance(config, str):
             config = mcpt.load_config(config)
         if config.get('use_pinyin', False):
-            model = Model(mcpt.models.MCPTPinyinModel(config))
+            model = cls(mcpt.models.MCPTPinyinModel(config))
         else:
-            model = Model(mcpt.models.MCPTModel(config))
+            model = cls(mcpt.models.MCPTModel(config))
         if load_model is not None:
             model.load_state_dict(torch.load(load_model, map_location=device))
         if device is not None:
