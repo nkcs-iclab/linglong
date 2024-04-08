a6a6f36ea99a653904b0c181f2e6b0adb42d9b43
Zhong Zhenyu <nczzy1997@gmail.com>
2024-04-09 04:53:18 +0800

    Addd LoRA support for training

--- training/train.py
+++ training/train.py
@@ -1,11 +1,13 @@
 import dataclasses
 
+import peft
 from transformers import (
     Trainer,
     HfArgumentParser,
     TrainingArguments,
 )
 from transformers.utils import check_min_version
+from peft import LoraConfig, TaskType
 
 import linglong
 import linglong.records
@@ -15,24 +17,42 @@ import linglong.records
 class ModelArguments:
     pretrained_model: str | None = dataclasses.field(
         default=None,
-        metadata={'help': 'Pretrained model path'},
+        metadata={'help': 'Pretrained model path.'},
     )
-
     model_config: str | None = dataclasses.field(
         default=None,
-        metadata={'help': 'Model config path'},
+        metadata={'help': 'Model config path.'},
+    )
+    use_peft_lora: bool = dataclasses.field(
+        default=False,
+        metadata={"help": 'Enables PEFT LoRA for training.'},
+    )
+    lora_alpha: int = dataclasses.field(
+        default=16,
+        metadata={'help': 'The scaling factor for the low-rank matrices.'},
+    )
+    lora_dropout: float = dataclasses.field(
+        default=0.1,
+        metadata={'help': 'The dropout probability of the LoRA layers.'},
+    )
+    lora_r: int = dataclasses.field(
+        default=64,
+        metadata={'help': 'The dimension of the low-rank matrices.'},
+    )
+    lora_target_modules: str = dataclasses.field(
+        default='c_attn,c_proj,c_fc',
+        metadata={'help': 'Comma separated list of target modules to apply LoRA layers to.'},
     )
 
 
 @dataclasses.dataclass
 class DataArguments:
     training_data: str = dataclasses.field(
-        metadata={'help': 'Training data path'},
+        metadata={'help': 'Training data path.'},
     )
-
     training_meta: str = dataclasses.field(
         default='train-meta.json',
-        metadata={'help': 'The meta file\'s filename of the training data'},
+        metadata={'help': 'The meta file\'s filename of the training data.'},
     )
 
 
@@ -55,6 +75,16 @@ def main():
         else:
             raise ValueError('Either pretrained_model or model_config must be provided.')
         model_config = model.config
+        if model_args.use_peft_lora:
+            lora_config = LoraConfig(
+                r=model_args.lora_r,
+                target_modules=model_args.lora_target_modules.split(','),
+                lora_alpha=model_args.lora_alpha,
+                lora_dropout=model_args.lora_dropout,
+                fan_in_fan_out=True,
+                task_type=TaskType.CAUSAL_LM,
+            )
+            model = peft.get_peft_model(model, lora_config)
         linglong.print_trainable_parameters(model, is_main_process=is_main_process, print_fn=spinner.write)
         spinner.write(model)
 
