f27b9ebb87006a49fb76b40520be4f6a77a101bd
Zhong Zhenyu <nczzy1997@gmail.com>
2024-05-16 13:40:44 +0800

    Move files

--- linglong/__init__.py
+++ linglong/__init__.py
@@ -1,11 +1,20 @@
-from linglong.legacy_modeling import Model
 from linglong.modeling_linglong import LingLongModel, LingLongForCausalLM, LingLongConfig
+
 import linglong.generation
+
 import linglong.tokenization_linglong
-from linglong.tokenization_linglong import LingLongTokenizer, PinyinTokenizer
+from linglong.tokenization_linglong import (
+    LingLongTokenizer,
+    PinyinTokenizer,
+)
 from linglong.tokenization_linglong_fast import LingLongTokenizerFast
 from linglong.tokenization_utils import get_tokenizers
+
 import linglong.data
+
 import linglong.datasets
+
+import linglong.compat
+from linglong.compat.modeling import Model
 
 from linglong.utils import *
---
+++ linglong/compat/__init__.py
@@ -0,0 +1,1 @@
+import linglong.compat.modeling
